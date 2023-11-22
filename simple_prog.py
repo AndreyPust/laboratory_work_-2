0cf2f543a9f2ebe5d5585b52373fb370ec90b593
Andrey <pustakovandrej@gmail.com>
2023-11-22 21:00:55 +0300

    add the comments

--- simple_prog.py
+++ simple_prog.py
@@ -1,8 +1,12 @@
-import math
+import math #for sqrt
+
 
 print("Введите стороны прямоугольника")
 length = int(input("Введите длину прямоугольника:"))
-width = int(input("Введите ширину прямоугольника:"))
+width = int(input("Введите ширину прямоугольника:")) #enter sides
+
+#defining a rectangle
+
 if length > width:
     print("Длина больше ширины")
     name_rectangle = "прямоугольника"
@@ -12,9 +16,12 @@ elif length < width:
 else:
     print("Длина равна ширине, данный прямоугольник является квадратом!")
     name_rectangle = "квадрата"
-perimeter_rectangle = (length + width)*2
+
+perimeter_rectangle = (length + width)*2 #perimeter search
 print("Периметр ", name_rectangle, "=", perimeter_rectangle)
-square_rectangle = length*width
+
+square_rectangle = length*width #area search
 print("Площадь ", name_rectangle, "=", square_rectangle)
-diagonal_main = math.sqrt(length*length + width*width)
+
+diagonal_main = math.sqrt(length*length + width*width) #diagonal search
 print("Диагональ ", name_rectangle, "=", diagonal_main)
