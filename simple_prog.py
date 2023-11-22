65d3bf78f7b9f5fc17adfcc2374eacec5eea7ce4
Andrey <pustakovandrej@gmail.com>
2023-11-22 20:40:53 +0300

    add square, perimetr, diagonal

--- simple_prog.py
+++ simple_prog.py
@@ -1,12 +1,20 @@
-print("Введите стороны прямоугольника:")
-length = input("Введите длину прямоугольника:")
-width = input("Введите ширину прямоугольника:")
+import math
+
+print("Введите стороны прямоугольника")
+length = int(input("Введите длину прямоугольника:"))
+width = int(input("Введите ширину прямоугольника:"))
 if length > width:
     print("Длина больше ширины")
-    name_rectangle = "прямоугольник"
+    name_rectangle = "прямоугольника"
 elif length < width:
     print("Длина меньше ширины")
-    name_rectangle = "прямоугольник"
+    name_rectangle = "прямоугольника"
 else:
     print("Длина равна ширине, данный прямоугольник является квадратом!")
-    name_rectangle = "квадрат"
+    name_rectangle = "квадрата"
+perimeter_rectangle = (length + width)*2
+print("Периметр ", name_rectangle, "=", perimeter_rectangle)
+square_rectangle = length*width
+print("Площадь ", name_rectangle, "=", square_rectangle)
+diagonal_main = math.sqrt(length*length + width*width)
+print("Диагональ ", name_rectangle, "=", diagonal_main)
