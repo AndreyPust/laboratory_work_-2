3543ce0f05d01c658a830a86cac754c0fef23702
Andrey <pustakovandrej@gmail.com>
2023-11-22 20:09:53 +0300

    add name_rectangle and fix

--- simple_prog.py
+++ simple_prog.py
@@ -3,5 +3,10 @@ length = input("Введите длину прямоугольника:")
 width = input("Введите ширину прямоугольника:")
 if length > width:
     print("Длина больше ширины")
-else:
+    name_rectangle = "прямоугольник"
+elif length < width:
     print("Длина меньше ширины")
+    name_rectangle = "прямоугольник"
+else:
+    print("Длина равна ширине, данный прямоугольник является квадратом!")
+    name_rectangle = "квадрат"
